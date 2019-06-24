b527eb7672ffe12f7a8e83473496b289bd4fb978
Axel <axosar@gmail.com>
2019-06-24 16:09:18 -0500

    COLOR-BOXES
    Color boxes starter

---
+++ src/Box.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './Box.css';
+
+class Box extends Component {
+  static defaultProps = {
+    allColors: ['purple','magenta','lilac','pink']
+  };
+
+  constructor(props){
+    super(props);
+    this.state = {
+      color: 'purple',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+
+  }
+
+  render(){
+    return(
+      <div 
+        className="Box" 
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}
+      >
+
+      </div>
+    )
+  }
+}
+
+export default Box;
