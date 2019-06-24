241ba2f94c4d4ae20d8e7e3d6b7128bde1da656d
Axel <axosar@gmail.com>
2019-06-24 16:28:01 -0500

    Finishing touches
    React finishing touches app color

--- src/Box.js
+++ src/Box.js
@@ -1,21 +1,26 @@
 import React, {Component} from 'react';
+import {choice} from './helpers';
 import './Box.css';
 
 class Box extends Component {
-  static defaultProps = {
-    allColors: ['purple','magenta','lilac','pink']
-  };
-
   constructor(props){
     super(props);
     this.state = {
-      color: 'purple',
+      color: choice(this.props.colors),
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(){
+  pickColor(){
+    let newColor;
+    do {
+      newColor = choice(this.props.colors);
+    } while (newColor === this.state.color )
+    this.setState({color: newColor});
+  }
 
+  handleClick(){
+    this.pickColor();
   }
 
   render(){
